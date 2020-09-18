3866c9e9ab95630e74fb1ac834956eea4eebc095
dmitry <evsvald@gmail.com>
2020-09-18 15:19:38 +0300

    ...

--- lesson3/task_3.2.py
+++
@@ -1,4 +0,0 @@
-def my_func(name, surname, byear, city, email, phone):
-    print(name, surname, byear, city, email, phone)
-
-my_func(name= 'Dmitry', surname='Kanev', byear=1991, city='city', email='email', phone='0638')
